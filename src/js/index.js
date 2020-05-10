5deaf3d0db4fd5bb8eb4efad495445bc18fa7684
julian-hecker <julianheckerdev@gmail.com>
2020-05-10 03:57:05 -0400

    add javascript template

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import Module from './module.js';
+import '../scss/index.scss';
+
---
+++ src/js/module.js
@@ -0,0 +1,16 @@
+// ****
+// Module
+// Does some stuff
+// ****
+
+export default class Module {
+    constructor() {
+
+        this.initEventListeners();
+    }
+
+    initEventListeners() {
+        // window.addEventListener('scroll');
+        // window.addEventListener('resize');
+    }
+}
